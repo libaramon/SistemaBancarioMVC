aaadcd19d90e4c1cc7de92a59faa7b154ba49850
libaramon <150966570+libaramon@users.noreply.github.com>
2026-05-18 17:46:49 -0600

    Add files via upload

---
+++ Models/Cliente.cs
@@ -0,0 +1,11 @@
+namespace SistemaBancarioMVC.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Saldo { get; set; }
+    }
+}
